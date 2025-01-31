6f27aaef463919f880224ae6d3a41462969c0673
Yakshi Batish <yakshi0726.becse24@chitkara.edu.in>
2025-01-31 10:30:20 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int A,B,C,D,F;
+    scanf("%d",&a,&b,&c,&d,&f);
+    if(A>=90){
+        printf("A");
+    }
+    else if(B>=80 && B<90){
+        printf("B");
+    }
+    else if(C>=70 && C<80){
+        printf("C");
+    }
+    else if(D>=60 && c<70){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
+}
