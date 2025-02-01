99db6c046ef56789043afc0261968fc2bda5cf70
Yakshi Batish <yakshi0726.becse24@chitkara.edu.in>
2025-02-01 18:42:08 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a==A){
+        printf("Excellent");
+    }
+    else if(a==B){
+        printf("Good");
+    }
+    else if(a==c){
+        printf("Average");
+    }
+    else if(a==d){
+        printf("Below Average");
+    }
+    else if(a==f){
+        printf("Fail");
+    }
+    else{
+        printf("Invalid grade");
+    }
+    
+}
