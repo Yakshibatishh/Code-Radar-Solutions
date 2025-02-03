01bb324962823d9e2e24de01f71b3d91da5db811
Yakshi Batish <yakshi0726.becse24@chitkara.edu.in>
2025-02-03 19:29:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main(){
     int a,b;
     char ch;
     scanf("%d %d %ch",&a,&b,&ch);
-    int addition,substraction,multiplication,division;
     if(ch=='+'){
         printf("%d",a+b);
     }
+}
