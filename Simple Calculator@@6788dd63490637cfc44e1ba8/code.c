a349db83921836a409c04eab8180c6d321ba536e
Yakshi Batish <yakshi0726.becse24@chitkara.edu.in>
2025-02-03 19:23:09 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    char ch;
+    scanf("%d %d %ch",&a,&b,&ch);
+    int addition,substraction,multiplication,division;
+    switch(0){
+        case'+':{
+            addition=a+b;
+            printf(%d\n,addition);
+            break;
+        }
+    }
+
+}
